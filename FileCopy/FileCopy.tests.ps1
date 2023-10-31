7ce5329edcdece89ec9bba31b919880f7c5d3991
Vivek Jindal <vivek.jindal@hcl.com>
2023-10-31 20:27:20 +0530

    pester module update

--- FileCopy/FileCopy.tests.ps1
+++ FileCopy/FileCopy.tests.ps1
@@ -28,13 +28,68 @@ Describe "Copy-BYFile" {
         }
     }
 
-    # it thorws an error when file is invalid or doesn't exist
+    # it thorws an error when file is doesn't exist
     Context "When source file does not exist" {
         $SourceFile = Join-Path -Path $PSScriptRoot -ChildPath "by.txt"
         $DestinationFile = Join-Path -Path $PSScriptRoot -ChildPath "by_copy.txt"
 
+        # run the copy test
         It "Throws an error" {
+            Write-Host "Executing Copy-BYFile command using test values..."
             { Copy-BYFile -SourcePath $SourceFile -DestinationPath $DestinationFile } | Should -Throw
+        }
+    }
+
+    # it throws an error when source is not a valid file
+    Context "When source is not a valid file" {
+        $SourceFile = Join-Path -Path $PSScriptRoot -ChildPath "by.txt"
+        $DestinationFile = Join-Path -Path $PSScriptRoot -ChildPath "by_copy.txt"
+
+        # create a source test file
+        BeforeAll {
+            Write-Host "Creating invalid file..."
+            New-Item -Path $SourceFile -ItemType Directory
+        }
+
+        # clean up the test files
+        AfterAll {
+            Write-Host "Cleaning up test files..."
+            Remove-Item -Path $SourceFile -Force
+            Remove-Item -Path $DestinationFile -Force -ErrorAction SilentlyContinue
+        }
+
+        # run the copy test
+        It "Throws an error" {
+            Write-Host "Executing Copy-BYFile command using test values..."
+            { Copy-BYFile -SourcePath $SourceFile -DestinationPath $DestinationFile } | Should -Throw
+        }
+    }
+
+    # it does not throw an error when destination file already exists
+    Context "When destination file already exists" {
+        $SourceFile = Join-Path -Path $PSScriptRoot -ChildPath "by.txt"
+        $DestinationFile = Join-Path -Path $PSScriptRoot -ChildPath "by_copy.txt"
+
+        # create source and destination test files
+        BeforeAll {
+            Write-Host "Creating invalid file..."
+            New-Item -Path $SourceFile -ItemType File
+            New-Item -Path $DestinationFile -ItemType File
+        }
+
+        # clean up the test files
+        AfterAll {
+            Write-Host "Cleaning up test files..."
+            Remove-Item -Path $SourceFile -Force
+            Remove-Item -Path $DestinationFile -Force
+        }
+
+        # run the copy test
+        It "Copies the file" {
+            Write-Host "Executing Copy-BYFile command using test values..."
+            Copy-BYFile -SourcePath $SourceFile -DestinationPath $DestinationFile
+            $FileCopiedResult = Test-Path -Path $DestinationFile -PathType Leaf
+            $FileCopiedResult | Should -Be $True
         }
     }
 
